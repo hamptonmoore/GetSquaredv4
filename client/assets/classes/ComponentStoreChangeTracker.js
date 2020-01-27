ab6ffefb986d73c2c7fd1d04203ff21b25aea1eb
Hampton Moore <me@hampton.pw>
2020-01-27 22:04:25 +0100

    remove duplicates from state changes list. This brought mutations per from 180 to 120. #5

--- client/assets/classes/ComponentStoreChangeTracker.js
+++ client/assets/classes/ComponentStoreChangeTracker.js
@@ -12,6 +12,31 @@ export class ComponentStoreChangeTracker {
         }
     }
 
+    // noinspection JSMethodCanBeStatic
+    removeDuplicates(arrayToChange){
+        arrayToChange = arrayToChange.reverse();
+
+        arrayToChange = arrayToChange.filter((item, currentIndex, originalArray)=>{
+
+            for (let checkIndex = 0; checkIndex < originalArray.length; checkIndex++){
+                if (originalArray[checkIndex][0] === item[0] && originalArray[checkIndex][1] === item[1] && originalArray[checkIndex][2] === item[2] && checkIndex < currentIndex){
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        return arrayToChange;
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    optimize(){
+        this.state.changes = this.removeDuplicates(this.state.changes);
+        this.state.creations = this.removeDuplicates(this.state.creations);
+        this.state.deletions = this.removeDuplicates(this.state.deletions);
+    }
+
     addChangeToComponent(componentName, entityID, key, value){
         this.state.changes.push([componentName, entityID, key, value]);
     }
